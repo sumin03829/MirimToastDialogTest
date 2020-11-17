d310ae582b869f344580c88072183118c6048dca
sumin_030829 <sumin0829@kako.com>
2020-11-17 19:56:55 +0900

    Complete Project Test1

--- app/src/main/java/kr/hs/emirim/s2019s38/mirimtoastdialogtest/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/s2019s38/mirimtoastdialogtest/MainActivity.java
@@ -3,11 +3,13 @@ package kr.hs.emirim.s2019s38.mirimtoastdialogtest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     TextView textName,textEmail;
@@ -25,7 +27,6 @@ public class MainActivity extends AppCompatActivity {
         textEmail=findViewById(R.id.text_email);
         editName=findViewById(R.id.edit_name);
         editEmail=findViewById(R.id.edth_email);
-        textToast=findViewById(R.id.text_toast);
         btnHere=findViewById(R.id.btn_here);
         btnHere.setOnClickListener(btnHerListener);
     }
@@ -36,10 +37,32 @@ public class MainActivity extends AppCompatActivity {
             AlertDialog.Builder dlg=new AlertDialog.Builder(MainActivity.this);
             dlg.setTitle("사용자 정보 입력");
             dlg.setView(dialogView);
-            dlg.setPositiveButton("확인",null);
-            dlg.setNegativeButton("취소",null);
+            dlg.setPositiveButton("확인",positiviButtonListener);
+            dlg.setNegativeButton("취소",negativeButtonListener);
             dlg.show();
 
+        }
+    };
+
+    DialogInterface.OnClickListener positiviButtonListener=new DialogInterface.OnClickListener() {
+        @Override
+        public void onClick(DialogInterface dialogInterface, int i) {
+            editName=dialogView.findViewById(R.id.edit_name);
+            editEmail=dialogView.findViewById(R.id.edth_email);
+            textName.setText(editName.getText().toString()+"님 입니다.");
+            textEmail.setText(editEmail.getText().toString());
+        }
+    };
+    DialogInterface.OnClickListener negativeButtonListener=new DialogInterface.OnClickListener() {
+        @Override
+        public void onClick(DialogInterface dialogInterface, int i) {
+            Toast toast=new Toast(MainActivity.this);
+            toastView=View.inflate(MainActivity.this,R.layout.toast1,null);
+            toast.setView(toastView);
+            textToast=toastView.findViewById(R.id.text_toast);
+            textToast.setText("취소버튼을 누르셨네요");
+            toast.setView(toastView);
+            toast.show();
         }
     };
 }
---
+++ app/src/main/java/kr/hs/emirim/s2019s38/mirimtoastdialogtest/MainActivity2.java
@@ -0,0 +1,14 @@
+package kr.hs.emirim.s2019s38.mirimtoastdialogtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
